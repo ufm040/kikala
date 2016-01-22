19a5ab44c14f01288f3c1d43cf888f913e156978
ufm040 <cmoinon@copar-info.fr>
2016-01-22 10:30:10 +0100

    Base de donnnées

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -4,12 +4,19 @@
 	<meta charset="UTF-8">
 	<title><?= $this->e($title) ?></title>
 
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/bootstrap.min.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
 </head>
 <body>
 	<div class="container">
 		<header>
-			<h1>W :: <?= $this->e($title) ?></h1>
+			<h1><a href="<?php echo $this->url('home')?>" title="Accueil">Kikala</a></h1>
+			<nav id="headmenu" class="clearfix">
+				<ul>
+					<li><a href="#" title="Formations">Formations en cours</a></li>
+					<li><a href="#" title="Créer une formation">Créer une formation</a></li>
+				</ul>
+			</nav>			
 		</header>
 
 		<section>
@@ -17,6 +24,15 @@
 		</section>
 
 		<footer>
+			<nav id="footermenu" class="clearfix">
+				<ul>
+					<li><a href="<?php echo $this->url('home')?>" title="Accueil">Accueil</a></li>
+					<li><a href="#" title="A propos">A propos</a></li>
+					<li><a href="#" title="Mentions Légales">Mentions Légales</a></li>
+					<li><a href="#" title="Inscription">Inscription</a></li>
+
+				</ul>
+			</nav>			
 		</footer>
 	</div>
 </body>
---
+++ wk3-kikala.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Ven 22 Janvier 2016 à 10:10
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `wk3-kikala`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `formations`
+--
+
+CREATE TABLE IF NOT EXISTS `formations` (
+  `id` int(11) NOT NULL,
+  `title` varchar(65) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dateFormation` datetime NOT NULL,
+  `place` varchar(65) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `duration` time(2) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `totalNumberPlace` int(3) NOT NULL,
+  `topAnnulation` tinyint(1) NOT NULL,
+  `dateCreated` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `userId` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `inscriptions`
+--
+
+CREATE TABLE IF NOT EXISTS `inscriptions` (
+  `userId` int(11) NOT NULL,
+  `formationId` int(11) NOT NULL,
+  PRIMARY KEY (`userId`,`formationId`),
+  KEY `formation_inscription` (`formationId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(65) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `birthday` year(4) NOT NULL,
+  `sexe` varchar(1) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `job` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `instructionDescription` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `studentDescription` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `credit` int(4) NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dateCreated` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `formations`
+--
+ALTER TABLE `formations`
+  ADD CONSTRAINT `user_formation` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `inscriptions`
+--
+ALTER TABLE `inscriptions`
+  ADD CONSTRAINT `formation_inscription` FOREIGN KEY (`formationId`) REFERENCES `formations` (`id`),
+  ADD CONSTRAINT `user_inscription` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
