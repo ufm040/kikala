5e5294f8f8130ab6ef617b48280b9a32eef61135
annenightgirl134 <anne-so.gesqua@hotmail.fr>
2016-02-01 14:29:07 +0100

    css liste formations

--- app/templates/formation/list_formations.php
+++ app/templates/formation/list_formations.php
@@ -1,29 +1,20 @@
-<?php $this->layout('layout', ['title' => 'Formations	']) ?>
+<?php $this->layout('layout', ['title' => 'Liste des formations']) ?>
 
 <?php $this->start('main_content') ?>
 	<h2>Liste des Formations</h2>
 
 	<div class="table-responsive">
 		<table class="table table-condensed">
-			<thead>
-				<tr>
-					<td>Titre</td>
-					<td>Date</td>
-					<td>Créé le</td>
-					<td>Lieu</td>
-					<td>Durée</td>
-					<td></td>
-				</tr>
-			</thead>
 			<tbody>
 				<?php foreach($formations as $formation) : ?>
 				<tr>
-					<td><?= $formation['title'] ?></td>
-					<td><?= $formation['dateFormation'] ?></td>
-					<td><?= $formation['dateCreated'] ?></td>
-					<td><?= $formation['city'] ?></td>
-					<td><?= $formation['duration'] ?></td>	
-					<td><a href="<?= $this->url('detail_formation', ['id' =>$formation['id']]) ?>" title="Voir le détail">Voir</a></td>			
+					<td><img src="<?= $this->assetUrl('img/formations/thumbnail/'.$formation['image']) ?>" alt="Image représentant la formation" class="img-thumbnail"></td>
+					<td><strong><?= $formation['title'] ?></strong></td>
+					<td><strong>Quand : </strong><?= $formation['dateFormation'] ?></td>
+					<td><strong>Durée : </strong><?= $formation['duration'] ?></td>
+					<td><strong>Lieu : </strong><?= $formation['city'] ?></td>
+					<td><strong>Créé le : </strong><?= $formation['dateCreated'] ?></td>
+					<td><a href="<?= $this->url('detail_formation', ['id' =>$formation['id']]) ?>" title="Voir le détail de la formation">Voir le détail de la formation</a></td>			
 				</tr>
 				<?php endforeach; ?>
 			</tbody>		
